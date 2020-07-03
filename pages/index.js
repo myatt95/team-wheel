158ace6c826076341e02cb000c06deea7c44e1be
Oliver Myatt <olivermyatt@rentalcars.com>
2020-07-03 09:52:07 +0100

    update names

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import Wheel from "../components/Wheel"
 import Sound from "react-sound"
 
 
-const people = ['JT', 'JT', 'JT', 'JT', 'JT', 'JT', 'JT'];
+const people = ['Oliver', 'Fiona', 'Sana', 'Mike', 'Kelly', 'JT'];
 
 export default function App() {
 
