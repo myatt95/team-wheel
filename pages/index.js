5231045ec08433dc610e1b7fa52fc5c57d78ef8e
FCastillo-91 <fionacastillo@rentalcars.com>
2020-12-15 14:50:24 +0000

    Christmasify the wheel

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,31 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import Wheel from "../components/Wheel"
 import Sound from "react-sound"
 
 
 const people = ['Oliver', 'Fiona', 'Sana', 'Mike', 'Andy'];
 
+const songs = ['donkey.mp3', "Jingle-Bells.mp3", "Santa.mp3", "Wham.mp3"];
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
 export default function App() {
 
-    const [status, setStatus] = useState(Sound.status.STOPPED)
+    const [status, setStatus] = useState(Sound.status.STOPPED);
+    const [randomSong, setRandomSong] = useState(songs[getRandomIntInclusive(0, songs.length - 1)]);
+
+    useEffect(() => {
+        setRandomSong(songs[getRandomIntInclusive(0, songs.length - 1)])
+    }, [status]);
 
     return (
         <div className="App">
             {typeof window !== "undefined" && (
-                <Sound url={`${window.location.href}Law-And-Order-Theme-Song.mp3`} playStatus={status}/>
+                <Sound url={`${window.location.href}${randomSong}`} playStatus={status}/>
             )}
             <h1>Who's the chosen one? 🤞</h1>
             <Wheel items={people} onClick={() => setStatus(Sound.status.PLAYING)}/>
